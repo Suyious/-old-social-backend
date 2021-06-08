9780def8f4ebf74e52ff9a17640419e2b9ef9422
Suyash <suyu162001@gmail.com>
2021-06-08 22:39:23 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const { gql, ApolloServer } = require("apollo-server");
+
+const typeDefs = gql`
+  type Query {
+    sayHi: String!
+  }
+`;
+
+const resolvers = {
+  Query: {
+    sayHi: () => "Hello World!",
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+server.listen({ port: 5000 }).then((res) => {
+  console.log(`Server running at ${res.url}`);
+});
