6d46b3f3ca0d87f2aa0a9f93977f0087345b3b86
Suyash <suyu162001@gmail.com>
2021-09-12 12:00:46 +0530

    made minor modififactions before deployment

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const typeDefs = require("./graphql/typeDefs");
 const resolvers = require("./graphql/resolvers");
 
 const pubsub =  new PubSub();
+const PORT = process.env.port || 5000;
 
 const server = new ApolloServer({
   typeDefs,
@@ -19,8 +20,11 @@ mongoose
   .connect(MONGO_DB, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log("MongoDB connected!");
-    return server.listen({ port: 5000 });
+    return server.listen({ port: PORT });
   })
   .then((res) => {
     console.log(`Server running at ${res.url}`);
-  });
+  }) 
+  .catch(err => {
+    console.err(err);
+  })
